a4a5fc95c52a6c2d787157c2a32f0dcbde15473b
Chee Ng <cwng@ncsu.edu>
2020-07-18 09:30:20 -0400

    Implemented Navbar & tested components and img display

---
+++ project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styles: []
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'Description', 'https://www.onegreenplanet.org/wp-content/uploads/2015/03/Fresh-Summer-Rolls-1200x800.jpg')
+  ];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
