041dcc5f14b322ec0fdf79734e440a792844b3d9
Chee Ng <cwng@ncsu.edu>
2020-07-18 11:03:33 -0400

    Partial of Shopping edit- buttons and input field

---
+++ project/src/app/shared/ingredient.model.ts
@@ -0,0 +1,11 @@
+export class Ingredient {
+    // Alternative way
+    // public name: string;
+    // public amount: number;
+
+    constructor(public name: string, public amount: number){
+        // No need this if using above way
+        // this.name = name;
+        // this.amount = amount;
+    }
+}
