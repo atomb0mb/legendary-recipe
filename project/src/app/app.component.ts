107d8ff2a75b7c711b390d6c13a5a2a49ccdad61
Chee Ng <cwng@ncsu.edu>
2020-07-24 04:01:46 -0400

    set up navigation for page display

--- project/src/app/app.component.ts
+++ project/src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'project';
+  loadedFeature = 'recipe';
+  //loaded
+
+  onNavigate(selected: string){
+    this.loadedFeature = selected;
+  }
 }
