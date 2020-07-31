0924eadcb71f0309b2643a00d46b006f3a7be5fb
Chee Ng <cwng@ncsu.edu>
2020-07-31 10:59:49 -0400

    Adding Ingredients to Recipes

--- project/src/app/recipes/recipe.service.ts
+++ project/src/app/recipes/recipe.service.ts
@@ -1,13 +1,19 @@
 import { Recipe } from './recipe.model';
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
 
   recipeSeleted = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-        new Recipe('A Test Recipe', 'Description 1', 'https://www.onegreenplanet.org/wp-content/uploads/2015/03/Fresh-Summer-Rolls-1200x800.jpg'),
-        new Recipe('Test Recipe 2', 'Test it', 'https://www.onegreenplanet.org/wp-content/uploads/2015/03/Fresh-Summer-Rolls-1200x800.jpg'),
+        new Recipe('A Test Recipe', 'Description 1', 'https://www.onegreenplanet.org/wp-content/uploads/2015/03/Fresh-Summer-Rolls-1200x800.jpg', [
+          new Ingredient('Banana', 10)
+        ]),
+        new Recipe('Test Recipe 2', 'Test it', 'https://www.onegreenplanet.org/wp-content/uploads/2015/03/Fresh-Summer-Rolls-1200x800.jpg', [
+          new Ingredient('Lemon', 12),
+          new Ingredient('Lettuce', 10) 
+        ]),
         
   ];
 
