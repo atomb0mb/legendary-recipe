4f127cbcdb247efb135f5b4b20bae206087bdace
Chee Ng <cwng@ncsu.edu>
2020-08-23 23:42:01 -0400

    Repaced viewChild emitter type of form to template driven

--- project/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ project/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { Ingredient } from 'src/app/shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -10,8 +11,8 @@ import { ShoppingListService } from '../shopping-list.service';
 })
 export class ShoppingEditComponent implements OnInit {
 
-  @ViewChild('nameInput') nameRef: ElementRef;
-  @ViewChild('amountInput', {static: false}) amountRef: ElementRef;
+  // @ViewChild('nameInput') nameRef: ElementRef;
+  // @ViewChild('amountInput', {static: false}) amountRef: ElementRef;
   //@Output() ingredientAdded = new EventEmitter<Ingredient>();
 
   constructor(private slService: ShoppingListService) { }
@@ -20,18 +21,18 @@ export class ShoppingEditComponent implements OnInit {
 
   }
 
-  addIngredient() {
-    const newIngredient = new Ingredient(this.nameRef.nativeElement.value, this.amountRef.nativeElement.value);
-    this.slService.addIngredient(newIngredient);
-   // this.ingredientAdded.emit(new Ingredient(this.nameRef.nativeElement.value, this.amountRef.nativeElement.value) );
-  }
-
   deleteIngredient() {
 
   }
 
   clearIngredient(){
 
+  }
+
+  onAddItem(form: NgForm){
+    const val = form.value;
+    const newIngredient = new Ingredient(val.name, val.amount);
+    this.slService.addIngredient(newIngredient);
   }
 
 }
