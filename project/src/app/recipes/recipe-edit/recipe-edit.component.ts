b624c7f2fcc7598b7fd9330e8c0c130b42d3a098
Chee Ng <cwng@ncsu.edu>
2020-08-26 01:14:23 -0400

    Implemented Validator

--- project/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ project/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
-import { FormGroup, FormControl, FormArray } from '@angular/forms';
+import { FormGroup, FormControl, FormArray, Validators } from '@angular/forms';
 import { RecipeService } from '../recipe.service';
 
 @Component({
@@ -45,17 +45,17 @@ export class RecipeEditComponent implements OnInit {
       if(recipe['ingredients']) {
         for(let ingredient of recipe.ingredients) {
           recipeIngredient.push( new FormGroup({
-            'name': new FormControl(ingredient.name),
-            'amount': new FormControl(ingredient.amount),
+            'name': new FormControl(ingredient.name, Validators.required),
+            'amount': new FormControl(ingredient.amount, [Validators.required, Validators.pattern(/^[1-9]+[0-9]*$/)]),
           }))
         } 
       }
     }
 
     this.recipeForm = new FormGroup({
-      'name': new FormControl(recipeName),
-      'imagePath': new FormControl(recipePath),
-      'description': new FormControl(recipeDes),
+      'name': new FormControl(recipeName, Validators.required),
+      'imagePath': new FormControl(recipePath, Validators.required),
+      'description': new FormControl(recipeDes, Validators.required),
       'ingredients': recipeIngredient,
     });
   }
@@ -68,8 +68,8 @@ export class RecipeEditComponent implements OnInit {
   // Remember to have type = button and not submit so that whenever you add ingredient, it wont submit too
   onAddIngredient() {
     (<FormArray>this.recipeForm.get('ingredients')).push(new FormGroup({
-      'name': new FormControl(),
-      'amount': new FormControl(),
+      'name': new FormControl(null, Validators.required),
+      'amount': new FormControl(null, [Validators.required, Validators.pattern(/^[1-9]+[0-9]*$/)]),
     }))
   }
 
