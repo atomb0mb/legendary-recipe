4cc7e53cdd3cf2fe76cc6c697ea637175113dac5
Chee Ng <cwng@ncsu.edu>
2020-08-26 01:57:31 -0400

    Submitting the recipe edit form

--- project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,25 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
 import { Router, ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styles: []
 })
-export class RecipeListComponent implements OnInit {
+export class RecipeListComponent implements OnInit, OnDestroy {
   recipes: Recipe[];
+  recipeSubscription: Subscription;
 
   constructor(private recipeService: RecipeService, private router: Router, private route: ActivatedRoute) {
     this.recipes = this.recipeService.getRecipes();
    }
 
   ngOnInit() {
+    this.recipeSubscription = this.recipeService.recipeChanged.subscribe(
+    (recipes: Recipe[])=> {
+      this.recipes = recipes;
+    })
   }
 
   onNewRecipe() {
     this.router.navigate(['new'], {relativeTo: this.route});
   }
+
+  ngOnDestroy(){
+    this.recipeSubscription.unsubscribe();
+  }
+  
 
 }
--- project/src/app/recipes/recipe.service.ts
+++ project/src/app/recipes/recipe.service.ts
@@ -2,10 +2,13 @@ import { Recipe } from './recipe.model';
 import { Injectable } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
+import { Subject } from 'rxjs';
 
 @Injectable() // you want to inject a service into a service
 export class RecipeService {
 
+
+  recipeChanged = new Subject<Recipe[]>();
 
   private recipes: Recipe[] = [
         new Recipe('A Test Recipe', 'Description 1', 'https://www.onegreenplanet.org/wp-content/uploads/2015/03/Fresh-Summer-Rolls-1200x800.jpg', [
@@ -34,5 +37,15 @@ export class RecipeService {
 
   addIngredientsToShoppingList(ingredients: Ingredient[]){
     this.slService.addAllIngredient(ingredients);
+  }
+
+  addRecipe(recipe: Recipe){
+    this.recipes.push(recipe);
+    this.recipeChanged.next(this.recipes.slice());
+  }
+
+  updateRecipe(index: number, newRecipe: Recipe){
+    this.recipes[index] = newRecipe;
+    this.recipeChanged.next(this.recipes.slice());
   }
 }
