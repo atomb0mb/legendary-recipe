1374f4bd1884941c89cd08d8c8472aa320e989b0
Chee Ng <cwng@ncsu.edu>
2020-08-26 03:03:13 -0400

    Implemented deletion for ingredients tab

--- project/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ project/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -100,5 +100,9 @@ export class RecipeEditComponent implements OnInit {
     this.router.navigate([''], {relativeTo: this.route})
   }
 
+  onRemoveIngredient(index: number) {
+    (<FormArray>this.recipeForm.get('ingredients')).removeAt(index);
+  }
+
 
 }
