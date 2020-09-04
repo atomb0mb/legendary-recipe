c1b6672655236a9291b63f91e2da9aa32a44100e
Chee Ng <cwng@ncsu.edu>
2020-09-04 00:48:05 -0400

    Fix bug of state that cause edit did not save

--- project/src/app/recipes/recipe-resolver.service.ts
+++ project/src/app/recipes/recipe-resolver.service.ts
@@ -2,13 +2,19 @@ import { Injectable } from '@angular/core'
 import { DataStorageService } from '../shared/data-storage.service'
 import { Recipe } from './recipe.model'
 import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router'
+import { RecipeService } from './recipe.service';
 
 @Injectable({ providedIn: 'root' })
 export class RecipeResolverService implements Resolve<Recipe[]> {
 
-    constructor(private dataService: DataStorageService) { }
+    constructor(private dataService: DataStorageService, private recipeService: RecipeService) { }
     resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-        return this.dataService.getRecipes();
+        const recipes = this.recipeService.getRecipes();
+        if(recipes.length == 0) {
+            return this.dataService.getRecipes();
+        } else {
+            return recipes;
+        }
         
     }
 }
