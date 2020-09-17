10adce2ca4eae8b132773d51e1677e80c6ccb92b
Chee Ng <cwng@ncsu.edu>
2020-09-17 00:03:51 -0400

    Expanding...the state

--- project/src/app/recipes/recipe.service.ts
+++ project/src/app/recipes/recipe.service.ts
@@ -5,7 +5,8 @@ import { Recipe } from './recipe.model';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
 import { Store } from '@ngrx/store';
-import * as ShoppingListActions from '../shopping-list/store/shopping-list.actions'
+import * as ShoppingListActions from '../shopping-list/store/shopping-list.actions';
+import * as FromShoppingList from '../shopping-list/store/shopping-list.reducer';
 
 @Injectable()
 export class RecipeService {
@@ -27,7 +28,7 @@ export class RecipeService {
   // ];
   private recipes: Recipe[] = [];
 
-  constructor(private slService: ShoppingListService, private store: Store<{ shoppingList: { ingredients: Ingredient[] } }>) {}
+  constructor(private slService: ShoppingListService, private store: Store<FromShoppingList.AppState>) {}
 
   setRecipes(recipes: Recipe[]) {
     this.recipes = recipes;
